52305c7c41fb4fef477a026647fed46f3c11ba03
fatihsamur <a.fatihsamur@gmail.com>
2022-05-30 04:44:12 +0300

    twoNumberSum

--- twoNumberSum.js
+++ twoNumberSum.js
@@ -1,7 +1,7 @@
-// given an array and  a target number, check if there is a pair of two number that their sum is equal to target number.
+// given an array and  a target number, check if there is a pair of two number in the array that their sum is equal to target number.
 
 // hash table solution, Time: O(N) Space: O(N)
-const isPairExist = (arr, num) => {
+const twoNumberSum = (arr, num) => {
   const length = arr.length;
   let hashTable = {};
   for (let i = 0; i < length; i++) {
@@ -15,5 +15,6 @@ const isPairExist = (arr, num) => {
   return false;
 };
 
-const res = isPairExist([3, 5, -4, 8, 11, 1, 4, 6], 10);
+/* Some Test Cases */
+const res = twoNumberSum([3, 5, -4, 8, 11, 1, 4, 6], 10);
 console.log(res);
